5be35d48924d7740100d3bb766fdc552b53ef7da
Edwin Moreno <edwinmoreno77@gmail.com>
2022-08-11 17:09:49 -0400

    HashLink added

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { HashLink as Link } from 'react-router-hash-link';
+
 
 export const Navbar = () => {
 
 
     return (
-        <>
+        <div id='Home'>
             <nav id="navbar-id" className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top ">
                 <div className="container">
                     <a href="https://www.instagram.com/fatima.afta/?hl=es" className="navbar-brand">
@@ -18,10 +18,10 @@ export const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mb-2 ms-auto mb-lg-0">
                             <li className="nav-item">
-                                <Link className='nav-link' to={`/homePage`}><i>Inicio</i></Link>
+                                <Link className='nav-link' to={`/homePage#Home`}><i>Inicio</i></Link>
                             </li>
                             <li className="nav-item">
-                                <Link className='nav-link' to={`/allcakes`}><i>Productos</i></Link>
+                                <Link className='nav-link' to={`/allcakes#subNavBar`}><i>Productos</i></Link>
                             </li>
                             <li className="nav-item">
                                 <a href="#Contactos" className="nav-link">
@@ -36,6 +36,6 @@ export const Navbar = () => {
                 </div>
 
             </nav>
-        </>
+        </div>
     )
 }
